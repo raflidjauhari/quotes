17476b6d5edf4361e219c262372f28dd9404f449
raflijauhari <rafli.llg2017@gmail.com>
2022-11-30 15:04:20 +0700

    quotes

---
+++ app/src/main/java/com/example/quotes/API/RetroServer.java
@@ -0,0 +1,12 @@
+package com.example.quotes.API;
+
+import retrofit2.Retrofit;
+
+public class RetroServer {
+    private static final String baseURL = "https://type.fit/api/";
+    private static Retrofit retro;
+
+    public static Retrofit connectRetrofit(){
+        
+    }
+}
---
+++ app/src/main/java/com/example/quotes/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.quotes.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.quotes.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

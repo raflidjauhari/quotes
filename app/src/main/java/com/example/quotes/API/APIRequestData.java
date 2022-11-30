5556b95af2449e0c81defa5d5fc462dc310b6def
raflijauhari <rafli.llg2017@gmail.com>
2022-11-30 15:19:59 +0700

    quotes

---
+++ app/src/main/java/com/example/quotes/API/APIRequestData.java
@@ -0,0 +1,13 @@
+package com.example.quotes.API;
+
+import com.example.quotes.Model.QuotesModel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface APIRequestData {
+    @GET("quotes")
+    Call<List<QuotesModel>> requestData();
+}
